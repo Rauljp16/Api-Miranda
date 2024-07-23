8a5cb3c1caf8f7d29e0f77a4948bb98888aa0e29
Rauljp16 <raul_astra16@hotmail.com>
2024-07-24 00:02:33 +0200

    instalacion de bcrypt

--- src/controllers/loginControllers.ts
+++ src/controllers/loginControllers.ts
@@ -1,14 +1,16 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { generateAccessToken } from "../services/loginServices";
 
+
 const router = Router()
 
-router.post("/", (req: Request, res: Response, _next: NextFunction) => {
+router.post("/", async (req: Request, res: Response, _next: NextFunction) => {
     const { username, password } = req.body;
 
     if (username === process.env.USER && password === process.env.PASSWORD) {
         const token = generateAccessToken(username);
         return res.json({ token })
+
     }
     return res.status(401).json({ message: "invalid credentials" })
 })
